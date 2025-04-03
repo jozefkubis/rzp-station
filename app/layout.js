efe76139d40637ef1e473db0d2c7e22798c4c3a2
JozefK <164421631+jozefkubis@users.noreply.github.com>
2025-04-03 14:19:02 +0200

    zalozenie projektu rzp-station

---
+++ app/layout.js
@@ -0,0 +1,30 @@
+import "@/app/_styles/globals.css"
+// import Header from "./_components/Header"
+import { Josefin_Sans } from "next/font/google"
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+})
+
+export const metadata = {
+  title: {
+    template: "%s RZP",
+    default: "Vitajte / RZP",
+  },
+  description:
+    "",
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`${josefin.className}`}>
+        {/* <Header /> */}
+        {/* <div className=""> */}
+        <main className="mx-auto w-full">{children}</main>
+        {/* </div> */}
+      </body>
+    </html>
+  )
+}
---
+++ app/page.js
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="text-4xl">RZP</h1>
+    </div>
+  )
+}
