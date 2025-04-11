f7ab20cdc38391f4896a434645d5a292abc5b547
jozef kubis <kubis.jozef@outlook.com>
2025-04-11 16:14:03 +0200

    Vytvoreny  a aplikovany SideBar, run build, spusteny vo vercel

---
+++ app/_components/SideBar.js
@@ -0,0 +1,18 @@
+import clsx from "clsx";
+import Link from "next/link";
+
+export default function SideBar({ link, isActive }) {
+  return (
+    <li
+      key={link.name}
+      className={clsx(
+        "rounded-md p-3 transition-transform duration-300 ease-in-out hover:bg-primary-50 active:scale-95",
+        { "bg-primary-50": isActive },
+      )}
+    >
+      <Link href={link.href} className="block h-full w-full">
+        {link.name}
+      </Link>
+    </li>
+  );
+}
--- app/calendar/page.js
+++ app/calendar/page.js
@@ -1,54 +1,42 @@
-import Link from "next/link"
-import Header from "../_components/Header"
-import clsx from "clsx"
-import { headers } from 'next/headers';
+import { headers } from "next/headers";
+import Header from "../_components/Header";
+import SideBar from "../_components/SideBar";
 
 export const metadata = {
-    title: "Kalendár",
-}
+  title: "Kalendár",
+};
 
 export default async function page() {
-    const headerData = headers(); // Zachytenie headers asynchrónne
-    const pathname = (await headerData)?.get('x-pathname') || ''; // Použitie await a náhradnej hodnoty
+  const headerData = headers(); // Zachytenie headers asynchrónne
+  const pathname = (await headerData)?.get("x-pathname") || ""; // Použitie await a náhradnej hodnoty
 
-    const navLinks = [
-        { name: "Test-1", href: "/calendar" },
-        { name: "Test-2", href: "/settings" },
-        { name: "Test-3", href: "/chat" },
-        { name: "Test-4", href: "/documents" },
-        { name: "Test-5", href: "/photos" },
-    ];
+  const navLinks = [
+    { name: "Test-1", href: "/calendar" },
+    { name: "Test-2", href: "/settings" },
+    { name: "Test-3", href: "/chat" },
+    { name: "Test-4", href: "/documents" },
+    { name: "Test-5", href: "/photos" },
+  ];
 
-    return (
-        <div>
-            <Header />
-            <div className="h-screen">
-                <div className="border-r border-primary-200 fixed left-0 top-0 h-screen w-[15rem] pt-[10rem]">
-                    <ul className="space-y-1 text-primary-700 font-semibold text-lg text-center px-4">
-                        {navLinks.map((link) => {
-                            const isActive = pathname === link.href;
+  return (
+    <div>
+      <Header />
+      <div className="h-screen">
+        <div className="fixed left-0 top-0 h-screen w-[15rem] border-r border-primary-200 pt-[10rem]">
+          <ul className="space-y-1 px-4 text-center text-lg font-semibold text-primary-700">
+            {navLinks.map((link) => {
+              const isActive = pathname === link.href;
 
-                            return (
-                                <li
-                                    key={link.name}
-                                    className={clsx(
-                                        'hover:bg-primary-50 p-3 rounded-md active:scale-95 transition-transform duration-300 ease-in-out',
-                                        { 'bg-primary-50': isActive }
-                                    )}
-                                >
-                                    <Link href={link.href} className="block w-full h-full">
-                                        {link.name}
-                                    </Link>
-                                </li>
-                            );
-                        })}
-                    </ul>
-                </div>
-                <div className="flex items-center justify-center pl-[15rem] h-full">
-                    <h1 className="text-8xl text-primary-700 font-bold">Kalendár</h1>
-                </div>
-            </div>
+              return (
+                <SideBar key={link.name} link={link} isActive={isActive} />
+              );
+            })}
+          </ul>
         </div>
-    );
+        <div className="flex h-full items-center justify-center pl-[15rem]">
+          <h1 className="text-8xl font-bold text-primary-700">Kalendár</h1>
+        </div>
+      </div>
+    </div>
+  );
 }
-
