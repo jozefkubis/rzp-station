72fe2f917143da02389d9e94afc2e425eb65d299
jozef kubis <kubis.jozef@outlook.com>
2025-04-17 18:37:20 +0200

    Pridanie balnkAvatar  a zvysenie limitu na foto

--- app/_components/UserHeaderInfo.js
+++ app/_components/UserHeaderInfo.js
@@ -1,46 +1,54 @@
-import { createClient } from "@/utils/supabase/server"
-import Image from "next/image"
-import Logo from "./Logo"
-import Link from "next/link"
+import { createClient } from "@/utils/supabase/server";
+import Image from "next/image";
+import Link from "next/link";
 
 export default async function UserHeaderInfo() {
-    const supabase = await createClient()
+  const supabase = await createClient();
 
-    const {
-        data: { user },
-    } = await supabase.auth.getUser()
+  const blankAvatar =
+    "https://kjfjavkvgocatxssthrv.supabase.co/storage/v1/object/public/avatars//1744906899450-avatar.png";
 
-    console.log(user);
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
+  const { data: profile, error } = await supabase
+    .from("profiles")
+    .select("avatar_url")
+    .eq("email", user?.email)
+    .single();
 
-    const { data: profile, error } = await supabase
-        .from("profiles")
-        .select("avatar_url")
-        .eq("email", user?.email)
-        .single()
+  if (error) {
+    console.error(error);
+    throw new Error("Profily nie je možné načítať");
+  }
 
-    if (error) {
-        console.error(error)
-        throw new Error("Profily nie je možné načítať")
-    }
+  const email = user?.email;
+  const avatarUrl = profile?.avatar_url;
+  console.log(avatarUrl);
 
-    const email = user?.email
-    const avatarUrl = profile?.avatar_url
-    console.log(avatarUrl);
-
-
-    return (
-        <Link href="/" className="flex items-center gap-4 ">
-            {avatarUrl ? (
-                <Image
-                    src={avatarUrl}
-                    height={100}
-                    width={100}
-                    alt="Avatar"
-                    className="hover:bg-primary-100 p-2 rounded-full active:scale-95 transition-transform duration-300 ease-in-out"
-                />
-            ) : <Logo />}
-            <span className="text-primary-700 font-semibold rounded-md p-4 transition-transform duration-300 ease-in-out hover:bg-primary-50 active:scale-95">{email}</span>
-        </Link>
-    )
+  return (
+    <Link href="/" className="flex items-center">
+      {avatarUrl ? (
+        <Image
+          src={avatarUrl}
+          height={100}
+          width={100}
+          alt="Avatar"
+          className="rounded-full p-2 transition-transform duration-300 ease-in-out hover:bg-primary-100 active:scale-95"
+        />
+      ) : (
+        <Image
+          src={blankAvatar}
+          height={100}
+          width={100}
+          alt="AvatarImage"
+          className="rounded-full p-2 transition-transform duration-300 ease-in-out hover:bg-primary-100 active:scale-95"
+        />
+      )}
+      <span className="rounded-md p-4 font-semibold text-primary-700 transition-transform duration-300 ease-in-out hover:bg-primary-50 active:scale-95">
+        {email}
+      </span>
+    </Link>
+  );
 }
--- app/page.js
+++ app/page.js
@@ -8,61 +8,50 @@ import {
 import { HiOutlinePhoto } from "react-icons/hi2";
 import { GiSkullWithSyringe } from "react-icons/gi";
 
+export const revalidate = 86400;
+
 export default function Page() {
   const navLinks = [
     {
       name: "Kalendár",
       href: "/calendar",
-      icon: (
-        <IoCalendarOutline className="h-20 w-20 text-primary-700" />
-      ),
-    }
-    ,
+      icon: <IoCalendarOutline className="h-20 w-20 text-primary-700" />,
+    },
     {
       name: "Fotky",
       href: "/photos",
-      icon: (
-        <HiOutlinePhoto className="h-20 w-20 text-primary-700" />
-      ),
+      icon: <HiOutlinePhoto className="h-20 w-20 text-primary-700" />,
     },
     {
       name: "Dokumenty",
       href: "/documents",
-      icon: (
-        <IoDocumentsOutline className="h-20 w-20 text-primary-700" />
-      ),
+      icon: <IoDocumentsOutline className="h-20 w-20 text-primary-700" />,
     },
     {
       name: "Debata",
       href: "/chat",
-      icon: (
-        <IoChatboxOutline className="h-20 w-20 text-primary-700" />
-      ),
+      icon: <IoChatboxOutline className="h-20 w-20 text-primary-700" />,
     },
     {
       name: "Nastavenia",
       href: "/settings",
-      icon: (
-        <IoSettingsOutline className="h-20 w-20 text-primary-700" />
-      ),
+      icon: <IoSettingsOutline className="h-20 w-20 text-primary-700" />,
     },
     {
       name: "Registrácia",
       href: "/register",
-      icon: (
-        <GiSkullWithSyringe className="h-20 w-20 text-primary-700" />
-      ),
+      icon: <GiSkullWithSyringe className="h-20 w-20 text-primary-700" />,
     },
   ];
 
   return (
-    <nav className="flex h-screen px-40 py-20 bg-gray-50">
+    <nav className="flex h-screen bg-gray-50 px-40 py-20">
       <ul className="grid h-full w-full grid-cols-3 items-center gap-4 text-center">
         {navLinks.map((link) => (
           <li key={link.name}>
             <Link
               href={link.href}
-              className="flex flex-col w-1/3 mx-auto items-center text-xl font-bold text-primary-700 hover:bg-primary-100 p-2 rounded-md active:scale-95 transition-transform duration-300 ease-in-out"
+              className="mx-auto flex w-1/3 flex-col items-center rounded-md p-2 text-xl font-bold text-primary-700 transition-transform duration-300 ease-in-out hover:bg-primary-100 active:scale-95"
             >
               {link.icon}
               {link.name}
