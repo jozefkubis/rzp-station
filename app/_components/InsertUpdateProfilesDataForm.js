b37c8a479d7b4706c6c5d1ca641a5e71cb36ec19
jozef kubis <kubis.jozef@outlook.com>
2025-04-24 16:33:15 +0200

    Pridany medCheckDate

--- app/_components/InsertUpdateProfilesDataForm.js
+++ app/_components/InsertUpdateProfilesDataForm.js
@@ -1,86 +1,100 @@
-"use client"
+"use client";
 
-import { useEffect, useState } from "react"
-import FormInput from "./FormInput"
-import UpdateProfileButton from "./UpdateProfileButton"
-import handleSubmitUploadProfileData from "../_lib/functions/handleSubmitUploadProfileData"
-import ImageUploader from "./ImageUploader"
-import toast from "react-hot-toast"
+import { useEffect, useState } from "react";
+import FormInput from "./FormInput";
+import UpdateProfileButton from "./UpdateProfileButton";
+import handleSubmitUploadProfileData from "../_lib/functions/handleSubmitUploadProfileData";
+import ImageUploader from "./ImageUploader";
+import toast from "react-hot-toast";
 
 function InsertUpdateProfilesDataForm({ profiles }) {
-    const [error, setError] = useState("")
-    const [full_name, setFull_name] = useState("")
-    const [username, setUsername] = useState("")
-    const [address, setAddress] = useState("")
-    const [dateOfBirth, setDateOfBirth] = useState("")
-    const [avatar, setAvatar] = useState(null)
+  const [error, setError] = useState("");
+  const [full_name, setFull_name] = useState("");
+  const [username, setUsername] = useState("");
+  const [address, setAddress] = useState("");
+  const [dateOfBirth, setDateOfBirth] = useState("");
+  const [medCheckDate, setMedCheckDate] = useState("");
+  const [avatar, setAvatar] = useState(null);
 
-    useEffect(() => {
-        if (error) toast.error(error)
-    }, [error])
+  useEffect(() => {
+    if (error) toast.error(error);
+  }, [error]);
 
+  function handleSubmit(e) {
+    e.preventDefault();
+    handleSubmitUploadProfileData(e, { setError, avatar });
+  }
 
-    function handleSubmit(e) {
-        e.preventDefault()
-        handleSubmitUploadProfileData(e, { setError, avatar })
-    }
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="w-full max-w-md space-y-6 rounded-lg p-8"
+    >
+      <div className="flex flex-col">
+        <FormInput
+          id="full_name"
+          type="text"
+          placeholder="Meno a priezvisko"
+          name="full_name"
+          onChange={(e) => setFull_name(e.target.value)}
+          value={full_name || profiles?.full_name || ""}
+          {...(!profiles && { required: true })}
+        />
+      </div>
 
-    return (
-        <form onSubmit={handleSubmit} className="w-full max-w-md rounded-lg p-8 space-y-6">
-            <div className="flex flex-col">
+      <div className="flex flex-col">
+        <FormInput
+          id="username"
+          type="text"
+          placeholder="Uživatelské meno"
+          name="username"
+          onChange={(e) => setUsername(e.target.value)}
+          value={username || profiles?.username || ""}
+          {...(!profiles && { required: true })}
+        />
+      </div>
 
-                <FormInput
-                    id="full_name"
-                    type="text"
-                    placeholder="Meno a priezvisko"
-                    name="full_name"
-                    onChange={(e) => setFull_name(e.target.value)}
-                    value={full_name || profiles?.full_name || ""}
-                    {...(!profiles && { required: true })}
-                />
-            </div>
+      <div className="flex flex-col">
+        <FormInput
+          id="address"
+          type="text"
+          placeholder="Adresa"
+          name="address"
+          onChange={(e) => setAddress(e.target.value)}
+          value={address || profiles?.address || ""}
+          {...(!profiles && { required: true })}
+        />
+      </div>
 
-            <div className="flex flex-col">
-                <FormInput
-                    id="username"
-                    type="text"
-                    placeholder="Uživatelské meno"
-                    name="username"
-                    onChange={(e) => setUsername(e.target.value)}
-                    value={username || profiles?.username || ""}
-                    {...(!profiles && { required: true })}
-                />
-            </div>
+      <div className="flex flex-col">
+        <FormInput
+          id="dateOfBirth"
+          type="date"
+          placeholder="Dátum narodenia"
+          name="dateOfBirth"
+          onChange={(e) => setDateOfBirth(e.target.value)}
+          value={dateOfBirth || profiles?.dateOfBirth || ""}
+          {...(!profiles && { required: true })}
+        />
+      </div>
 
-            <div className="flex flex-col">
-                <FormInput
-                    id="address"
-                    type="text"
-                    placeholder="Adresa"
-                    name="address"
-                    onChange={(e) => setAddress(e.target.value)}
-                    value={address || profiles?.address || ""}
-                    {...(!profiles && { required: true })}
-                />
-            </div>
+      <div className="flex flex-col">
+        <FormInput
+          id="medCheckDate"
+          type="date"
+          placeholder="Dátum prehliadky"
+          name="medCheckDate"
+          onChange={(e) => setMedCheckDate(e.target.value)}
+          value={medCheckDate || profiles?.medCheckDate || ""}
+          {...(!profiles && { required: true })}
+        />
+      </div>
 
-            <div className="flex flex-col">
-                <FormInput
-                    id="dateOfBirth"
-                    type="date"
-                    placeholder="Dátum narodenia"
-                    name="dateOfBirth"
-                    onChange={(e) => setDateOfBirth(e.target.value)}
-                    value={dateOfBirth || profiles?.dateOfBirth || ""}
-                    {...(!profiles && { required: true })}
-                />
-            </div>
+      <ImageUploader onAvatarSelect={setAvatar} />
 
-            <ImageUploader onAvatarSelect={setAvatar} />
-
-            <UpdateProfileButton />
-        </form>
-    )
+      <UpdateProfileButton />
+    </form>
+  );
 }
 
-export default InsertUpdateProfilesDataForm
+export default InsertUpdateProfilesDataForm;
--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,241 +1,234 @@
-'use server'
+"use server";
 
-import { revalidatePath } from 'next/cache'
-import { redirect } from 'next/navigation'
-import { createClient } from '@/utils/supabase/server'
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { createClient } from "@/utils/supabase/server";
 
 export async function login(formData) {
-    const supabase = await createClient()
+  const supabase = await createClient();
 
-    const data = {
-        email: formData.get('email'),
-        password: formData.get('password'),
-    }
+  const data = {
+    email: formData.get("email"),
+    password: formData.get("password"),
+  };
 
-    const { error } = await supabase.auth.signInWithPassword(data)
+  const { error } = await supabase.auth.signInWithPassword(data);
 
-    if (error) {
-        redirect('/error')
-    }
+  if (error) {
+    redirect("/error");
+  }
 
-    revalidatePath('/', 'layout')
-    redirect('/')
+  revalidatePath("/", "layout");
+  redirect("/");
 }
 
 export async function logout() {
-    const supabase = await createClient()
+  const supabase = await createClient();
 
-    await supabase.auth.signOut()
-    revalidatePath('/', 'layout')
-    redirect('/login')
+  await supabase.auth.signOut();
+  revalidatePath("/", "layout");
+  redirect("/login");
 }
 
 export async function signup(formData) {
-    const supabase = await createClient();
+  const supabase = await createClient();
 
-    const email = formData.get("email");
-    const password = formData.get("password");
-    const re_password = formData.get("re_password");
+  const email = formData.get("email");
+  const password = formData.get("password");
+  const re_password = formData.get("re_password");
 
-    // Overenie, či sú všetky polia vyplnené
-    if (!email || !password || !re_password) {
-        return { error: "Všetky polia sú povinné." };
-    }
+  // Overenie, či sú všetky polia vyplnené
+  if (!email || !password || !re_password) {
+    return { error: "Všetky polia sú povinné." };
+  }
 
-    // Skontroluj, či už existuje profil s daným e-mailom
-    const { data: existingProfile, error: profilesError } = await supabase
-        .from("profiles")
-        .select("email")
-        .eq("email", email)
-        .single();
+  // Skontroluj, či už existuje profil s daným e-mailom
+  const { data: existingProfile, error: profilesError } = await supabase
+    .from("profiles")
+    .select("email")
+    .eq("email", email)
+    .single();
 
-    // Ak nastane iná chyba ako "záznam sa nenašiel", ohlás chybu
-    if (profilesError && profilesError.code !== "PGRST116") {
-        console.error(profilesError);
-        return { error: "Email nie je možné načítať" };
-    }
+  // Ak nastane iná chyba ako "záznam sa nenašiel", ohlás chybu
+  if (profilesError && profilesError.code !== "PGRST116") {
+    console.error(profilesError);
+    return { error: "Email nie je možné načítať" };
+  }
 
-    // Ak používateľ s týmto emailom už existuje
-    if (existingProfile) {
-        return { error: "Užívateľ s týmto emailom už existuje" };
-    }
+  // Ak používateľ s týmto emailom už existuje
+  if (existingProfile) {
+    return { error: "Užívateľ s týmto emailom už existuje" };
+  }
 
-    // Kontrola, či sa heslá zhodujú
-    if (password !== re_password) {
-        return { error: "Heslá sa nezhodujú." };
-    }
+  // Kontrola, či sa heslá zhodujú
+  if (password !== re_password) {
+    return { error: "Heslá sa nezhodujú." };
+  }
 
-    // Kontrola minimálnej dĺžky hesla
-    if (password.length < 6) {
-        return { error: "Heslo musí byť dlhšie ako 6 znakov." };
-    }
+  // Kontrola minimálnej dĺžky hesla
+  if (password.length < 6) {
+    return { error: "Heslo musí byť dlhšie ako 6 znakov." };
+  }
 
-    // Registrácia používateľa
-    const { data: result, error } = await supabase.auth.signUp({
-        email,
-        password,
-    });
+  // Registrácia používateľa
+  const { data: result, error } = await supabase.auth.signUp({
+    email,
+    password,
+  });
 
-    if (error) {
-        console.error("Chyba pri registrácii:", error);
-        return { error: error.message };
-    }
+  if (error) {
+    console.error("Chyba pri registrácii:", error);
+    return { error: error.message };
+  }
 
-    // Ošetrenie neautorizovaného používateľa (napr. pri probléme s tokenom)
-    if (error?.status === 401) {
-        await supabase.auth.signOut();
-        redirect("/login");
-    }
+  // Ošetrenie neautorizovaného používateľa (napr. pri probléme s tokenom)
+  if (error?.status === 401) {
+    await supabase.auth.signOut();
+    redirect("/login");
+  }
 
-    // Ak používateľ ešte nemá session – pošli ho overiť e-mail
-    if (!result.session) {
-        revalidatePath("/", "layout");
-        redirect("/verify-email");
-    }
-
-    // V opačnom prípade – úspešná registrácia, presmeruj domov
+  // Ak používateľ ešte nemá session – pošli ho overiť e-mail
+  if (!result.session) {
     revalidatePath("/", "layout");
-    redirect("/");
+    redirect("/verify-email");
+  }
+
+  // V opačnom prípade – úspešná registrácia, presmeruj domov
+  revalidatePath("/", "layout");
+  redirect("/");
 }
 
-
-
 export async function InsertUpdateProfilesDataForm(formData) {
-    const supabase = await createClient();
+  const supabase = await createClient();
 
-    // 🔐 Získame aktuálne prihláseného používateľa
-    const {
-        data: { user },
-        error: userError,
-    } = await supabase.auth.getUser();
+  // 🔐 Získame aktuálne prihláseného používateľa
+  const {
+    data: { user },
+    error: userError,
+  } = await supabase.auth.getUser();
 
-    if (userError || !user) {
-        console.error("Nie je prihlásený používateľ", userError);
-        redirect("/login");
-        return;
+  if (userError || !user) {
+    console.error("Nie je prihlásený používateľ", userError);
+    redirect("/login");
+    return;
+  }
+
+  // 🧾 Získanie aktuálneho profilu z databázy
+  const { data: existingProfile, error: profileError } = await supabase
+    .from("profiles")
+    .select("*")
+    .eq("id", user.id)
+    .single();
+
+  if (profileError && profileError.code !== "PGRST116") {
+    console.error("Chyba pri načítaní profilu:", profileError);
+    return { error: "Nepodarilo sa načítať profil." };
+  }
+
+  const newData = {
+    full_name: formData.get("full_name"),
+    username: formData.get("username"),
+    address: formData.get("address"),
+    dateOfBirth: formData.get("dateOfBirth"),
+    medCheckDate: formData.get("medCheckDate"),
+  };
+
+  // 🎯 Zistíme, ktoré polia sa skutočne zmenili
+  const updatedFields = {};
+  for (const key in newData) {
+    if (newData[key] && newData[key] !== existingProfile?.[key]) {
+      updatedFields[key] = newData[key];
+    }
+  }
+
+  // 🖼 Práca s obrázkom (avatar)
+  let avatar = formData.get("avatar");
+
+  if (avatar && avatar instanceof File) {
+    const avatarName = `${Date.now()}-${avatar.name}`.replace(/\s/g, "-");
+
+    // ☁️ Upload obrázka do Supabase storage (bucket: "avatars")
+    const { data: uploadData, error: uploadError } = await supabase.storage
+      .from("avatars")
+      .upload(avatarName, avatar, { cacheControl: "3600", upsert: true });
+
+    if (uploadError) {
+      console.error("Chyba pri nahrávaní obrázka:", uploadError);
+      return { error: "Nepodarilo sa nahrať obrázok." };
     }
 
-    // 🧾 Získanie aktuálneho profilu z databázy
-    const { data: existingProfile, error: profileError } = await supabase
-        .from("profiles")
-        .select("*")
-        .eq("id", user.id)
-        .single();
+    // 🌐 Vytvorenie URL k nahratému obrázku
+    const avatarPath = `https://kjfjavkvgocatxssthrv.supabase.co/storage/v1/object/public/avatars/${avatarName}`;
 
-    if (profileError && profileError.code !== "PGRST116") {
-        console.error("Chyba pri načítaní profilu:", profileError);
-        return { error: "Nepodarilo sa načítať profil." };
-    }
+    // 🔗 Pridáme URL obrázka do dát pre databázu
+    updatedFields.avatar_url = avatarPath;
+  }
 
-    const newData = {
-        full_name: formData.get("full_name"),
-        username: formData.get("username"),
-        address: formData.get("address"),
-        dateOfBirth: formData.get("dateOfBirth"),
-    };
+  // ⛔ Ak nie sú žiadne zmeny, netreba robiť nič
+  if (Object.keys(updatedFields).length === 0) {
+    console.log("Žiadne zmeny sa nenašli, nič neupdatujem.");
+    return;
+  }
 
-    // 🎯 Zistíme, ktoré polia sa skutočne zmenili
-    const updatedFields = {};
-    for (const key in newData) {
-        if (
-            newData[key] &&
-            newData[key] !== existingProfile?.[key]
-        ) {
-            updatedFields[key] = newData[key];
-        }
-    }
+  // 💾 Uložime len zmenené polia
+  const { error } = await supabase
+    .from("profiles")
+    .upsert(
+      {
+        id: user.id,
+        ...updatedFields,
+      },
+      { onConflict: "id" },
+    )
+    .single();
 
-    // 🖼 Práca s obrázkom (avatar)
-    let avatar = formData.get("avatar");
+  if (error) {
+    console.error("Chyba pri upserte profilu:", error);
+    redirect("/error");
+  }
 
-    if (avatar && avatar instanceof File) {
-        const avatarName = `${Date.now()}-${avatar.name}`.replace(/\s/g, "-");
-
-        // ☁️ Upload obrázka do Supabase storage (bucket: "avatars")
-        const { data: uploadData, error: uploadError } = await supabase.storage
-            .from("avatars")
-            .upload(avatarName, avatar, { cacheControl: "3600", upsert: true });
-
-        if (uploadError) {
-            console.error("Chyba pri nahrávaní obrázka:", uploadError);
-            return { error: "Nepodarilo sa nahrať obrázok." };
-        }
-
-        // 🌐 Vytvorenie URL k nahratému obrázku
-        const avatarPath = `https://kjfjavkvgocatxssthrv.supabase.co/storage/v1/object/public/avatars/${avatarName}`;
-
-        // 🔗 Pridáme URL obrázka do dát pre databázu
-        updatedFields.avatar_url = avatarPath;
-    }
-
-
-    // ⛔ Ak nie sú žiadne zmeny, netreba robiť nič
-    if (Object.keys(updatedFields).length === 0) {
-        console.log("Žiadne zmeny sa nenašli, nič neupdatujem.");
-        return;
-    }
-
-    // 💾 Uložime len zmenené polia
-    const { error } = await supabase
-        .from("profiles")
-        .upsert(
-            {
-                id: user.id,
-                ...updatedFields,
-            },
-            { onConflict: "id" }
-        )
-        .single();
-
-    if (error) {
-        console.error("Chyba pri upserte profilu:", error);
-        redirect("/error");
-    }
-
-
-    revalidatePath("/", "layout");
+  revalidatePath("/", "layout");
 }
 
 export async function updateUser(formData) {
-    const supabase = await createClient();
+  const supabase = await createClient();
 
-    const newPassword = formData.get("newPassword");
-    const re_newPassword = formData.get("re_newPassword");
+  const newPassword = formData.get("newPassword");
+  const re_newPassword = formData.get("re_newPassword");
 
-    if (!newPassword) {
-        return { error: "Heslo nemôže byť prázdne." };
+  if (!newPassword) {
+    return { error: "Heslo nemôže byť prázdne." };
+  }
+
+  if (newPassword.length < 6) {
+    return { error: "Heslo musí mať aspoň 6 znakov." };
+  }
+
+  if (newPassword !== re_newPassword) {
+    return { error: "Heslá sa nezhodujú." };
+  }
+
+  const { data: result, error } = await supabase.auth.updateUser({
+    password: newPassword,
+  });
+
+  if (error) {
+    console.error("Chyba pri zmene hesla:", error);
+
+    // špeciálne ošetrenie pre neautorizovaného používateľa
+    if (error.status === 401) {
+      await supabase.auth.signOut();
+      redirect("/login");
     }
 
-    if (newPassword.length < 6) {
-        return { error: "Heslo musí mať aspoň 6 znakov." };
-    }
+    return { error: error.message };
+  }
 
-    if (newPassword !== re_newPassword) {
-        return { error: "Heslá sa nezhodujú." };
-    }
-
-    const { data: result, error } = await supabase.auth.updateUser({
-        password: newPassword,
-    });
-
-    if (error) {
-        console.error("Chyba pri zmene hesla:", error);
-
-        // špeciálne ošetrenie pre neautorizovaného používateľa
-        if (error.status === 401) {
-            await supabase.auth.signOut();
-            redirect("/login");
-        }
-
-        return { error: error.message };
-    }
-
-    if (!result.session) {
-        await supabase.auth.signOut();
-        revalidatePath("/", "layout");
-        redirect("/login");
-    }
-
+  if (!result.session) {
+    await supabase.auth.signOut();
     revalidatePath("/", "layout");
-}
+    redirect("/login");
+  }
 
+  revalidatePath("/", "layout");
+}
