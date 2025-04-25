cd147b10433a6019215e7ba12ff161db9685aa34
jozef kubis <kubis.jozef@outlook.com>
2025-04-25 17:11:54 +0200

    Pridany toast do handleSubmitUpdateUserData

--- app/_lib/functions/handleSubmitUpdateUserData.js
+++ app/_lib/functions/handleSubmitUpdateUserData.js
@@ -1,14 +1,17 @@
-import { updateUser } from "../actions"
+import toast from "react-hot-toast";
+import { updateUser } from "../actions";
 
 export default async function handleSubmitUpdateUserData(e, { setError }) {
-    e.preventDefault()
+  e.preventDefault();
 
-    const formData = new FormData(e.currentTarget)
-    const response = await updateUser(formData)
+  const formData = new FormData(e.currentTarget);
+  const response = await updateUser(formData);
 
-    if (response?.error) {
-        setError(response.error)
-    } else {
-        e.target.reset()
-    }
+  if (response?.error) {
+    setError(response.error);
+    toast.error(response.error);
+  } else {
+    e.target.reset();
+    toast.success("Profil bol aktualizovaný.");
+  }
 }
