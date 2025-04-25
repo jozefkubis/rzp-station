0fe747e35f7d07bf451ba92c952a6bcacbd1dbb9
jozef kubis <kubis.jozef@outlook.com>
2025-04-25 16:35:19 +0200

    Uprava handlosubmitlogin a loginform

--- app/_lib/functions/handleSubmitLogin.js
+++ app/_lib/functions/handleSubmitLogin.js
@@ -1,11 +1,15 @@
-import { login } from "../actions"
+"use client";
+
+import { login } from "../_actions/login";
 
 export default async function handleSubmitLogin(e, { setError }) {
-    e.preventDefault()
-    const formData = new FormData(e.target)
-    const response = await login(formData)
+  e.preventDefault();
 
-    if (response?.error) {
-        setError(response.error)
-    }
+  const formData = new FormData(e.target);
+  const result = await login(formData);
+
+  if (result?.error) {
+    setError(result.error);
+    return;
+  }
 }
