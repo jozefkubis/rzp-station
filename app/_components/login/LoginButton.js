8c5d7a304997c82c530cc53d3f32ec0c8fde1777
JozefK <164421631+jozefkubis@users.noreply.github.com>
2025-06-03 22:31:08 +0200

    oprava preklepu v login.button

--- app/_components/login/LoginButton.js
+++ app/_components/login/LoginButton.js
@@ -8,7 +8,7 @@ export default function LoginButton() {
     <button
       type="submit"
       disabled={pending}
-      className="disable:bg-creamy-300 bg-quaternary-900 text-primary-50 font-semibold py-3 rounded-md hover:bg-quaternary-800 transition my-4 w-full active:scale-95 text-xl"
+      className="disabled:bg-creamy-300 bg-quaternary-900 text-primary-50 font-semibold py-3 rounded-md hover:bg-quaternary-800 transition my-4 w-full active:scale-95 text-xl"
     >
       {pending ? <SpinnerMini /> : "Prihlásiť sa"}
     </button>
