9799bc4549111388ebbef6bf174ad8ea1da8fdab
JozefK <164421631+jozefkubis@users.noreply.github.com>
2025-07-01 14:16:34 +0200

    Pridany ConfirmDelete do DeleteAllShifts.js

--- app/_components/shifts/DeleteAllShifts.js
+++ app/_components/shifts/DeleteAllShifts.js
@@ -1,27 +1,51 @@
 "use client";
 
+import { useState } from "react";
 import Button from "../Button";
 import { clearMonth } from "@/app/_lib/actions";
+import Modal from "../Modal";
+import ConfirmDelete from "../ConfirmDelete";
 // import { useRouter } from "next/navigation";
 
 export default function DeleteAllShifts() {
-  // const router = useRouter();
+  const [isOpenDeleteModal, setIsOpenDeleteModal] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
 
   const today = new Date();
   const year = today.getFullYear();
   const month = today.getMonth() + 1;
 
-  async function handleClearMonth() {
-    if (!confirm("Naozaj vymazať všetky služby v aktuálnom mesiaci?")) return;
-    await clearMonth(year, month);
-    // router.refresh();
+
+  function handleOpenModal() {
+    setIsOpenDeleteModal(true);
   }
 
+  async function handleConfirmDelete() {
+    setIsDeleting(true);
+    await clearMonth(year, month);
+    setIsDeleting(false);
+    setIsOpenDeleteModal(false);
+  }
+
+
   return (
-    <div className="mt-6 self-start 2xl:px-32">
-      <Button variant="danger" onClick={handleClearMonth}>
-        🧹 Vymaž celý mesiac
-      </Button>
-    </div>
+    <>
+      <div className="mt-6 self-start 2xl:px-32">
+        <Button variant="danger" onClick={handleOpenModal}>
+          🧹 Vymaž celý mesiac"
+        </Button>
+      </div>
+
+      {isOpenDeleteModal && (
+        <Modal onClose={() => setIsOpenDeleteModal(false)}>
+          <ConfirmDelete
+            resourceName="Všetky služby"
+            onConfirm={handleConfirmDelete}
+            onClose={() => setIsOpenDeleteModal(false)}
+            disabled={isDeleting}
+          />
+        </Modal>
+      )}
+    </>
   );
 }
