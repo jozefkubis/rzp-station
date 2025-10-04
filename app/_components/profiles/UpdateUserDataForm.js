cc5891823dc892593852bc7f08a5f98eefff68f1
jozef kubis <kubis.jozef@outlook.com>
2025-10-04 07:24:34 +0200

    Pridany SpnnerMini.js pre zmenu hesla

--- app/_components/profiles/UpdateUserDataForm.js
+++ app/_components/profiles/UpdateUserDataForm.js
@@ -1,64 +1,82 @@
-"use client"
+"use client";
 
-import { useEffect, useState } from "react"
-import FormInput from "@/app/_components/FormInput"
-import handleSubmitUpdateUserData from "@/app/_lib/functions/handleSubmitUpdateUserData"
-import toast from "react-hot-toast"
-import Button from "@/app/_components/Button"
+import Button from "@/app/_components/Button";
+import FormInput from "@/app/_components/FormInput";
+import handleSubmitUpdateUserData from "@/app/_lib/functions/handleSubmitUpdateUserData";
+import { useEffect, useState, useTransition } from "react";
+import toast from "react-hot-toast";
+import SpinnerMini from "../SpinnerMini";
 
 function UpdateUserDataForm({ user }) {
-    const [error, setError] = useState("")
+  const [error, setError] = useState("");
+  const [isPending, startTransition] = useTransition();
 
-    useEffect(() => {
-        if (error) toast.error(error)
-    }, [error])
+  useEffect(() => {
+    if (error) toast.error(error);
+  }, [error]);
 
-    function handleSubmit(e) {
-        handleSubmitUpdateUserData(e, { setError })
-    }
+  function handleSubmit(e) {
+    startTransition(() => {
+      handleSubmitUpdateUserData(e, { setError });
+    });
+  }
 
-    return (
-        <form onSubmit={handleSubmit} className="rounded-lg p-8 flex flex-col justify-center h-screen w-1/2 mx-auto">
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="mx-auto flex h-screen w-1/2 flex-col justify-center rounded-lg p-8"
+    >
+      <div className="flex flex-col">
+        <FormInput
+          label="Email"
+          id="email"
+          type="email"
+          placeholder="example@email.com"
+          name="email"
+          disabled={true}
+          defaultValue={user?.email}
+        />
+      </div>
 
-            <div className="flex flex-col">
-                <FormInput
-                    label="Email"
-                    id="email"
-                    type="email"
-                    placeholder="example@email.com"
-                    name="email"
-                    disabled={true}
-                    defaultValue={user?.email}
-                />
-            </div>
+      <div className="flex flex-col">
+        <FormInput
+          label="Nové heslo"
+          id="newPassword"
+          type="password"
+          placeholder="Vaše nové heslo"
+          name="newPassword"
+          required
+        />
+      </div>
 
-            <div className="flex flex-col">
-                <FormInput
-                    label="Nové heslo"
-                    id="newPassword"
-                    type="password"
-                    placeholder="Vaše nové heslo"
-                    name="newPassword"
-                    required
-                />
-            </div>
+      <div className="flex flex-col">
+        <FormInput
+          label="Potvrdenie hesla"
+          id="re-newPassword"
+          type="password"
+          placeholder="Potvrdenie hesla"
+          name="re_newPassword"
+          required
+        />
+      </div>
 
-            <div className="flex flex-col">
-                <FormInput
-                    label="Potvrdenie hesla"
-                    id="re-newPassword"
-                    type="password"
-                    placeholder="Potvrdenie hesla"
-                    name="re_newPassword"
-                    required
-                />
-            </div>
-
-            <div className="w-full flex justify-end py-10">
-                <Button variant="primary" size="large">Aktualizovať heslo</Button>
-            </div>
-        </form>
-    )
+      <div className="flex w-full justify-end py-10">
+        <Button variant="primary" size="large">
+          {isPending ? (
+            <>
+              Aktualizujem{" "}
+              <span>
+                {" "}
+                <SpinnerMini />
+              </span>
+            </>
+          ) : (
+            "Aktualizovať heslo"
+          )}
+        </Button>
+      </div>
+    </form>
+  );
 }
 
-export default UpdateUserDataForm
+export default UpdateUserDataForm;
