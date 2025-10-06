685c16b0684c220a3aed51a6dcfccefbc22f71ec
JozefK <164421631+jozefkubis@users.noreply.github.com>
2025-10-06 06:49:25 +0200

    Zosychron. ShiftLoader.js s Modal Overlay

--- app/_components/shifts/ShiftLoader.js
+++ app/_components/shifts/ShiftLoader.js
@@ -2,7 +2,7 @@ import Spinner from "../Spinner"
 
 export default function ShiftLoader() {
     return (
-        <div className="fixed inset-0 z-50 bg-primary-50 bg-opacity-30 backdrop-blur-sm transition-all duration-500">
+        <div className="fixed inset-0 z-50 bg-black/40 backdrop-blur-sm transition-opacity duration-300">
             <div className="flex justify-center items-center h-screen w-full">
                 <Spinner />
             </div>
