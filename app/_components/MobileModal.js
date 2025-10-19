579138118766e7a06d71790f506f06b1f0241b79
JozefK <164421631+jozefkubis@users.noreply.github.com>
2025-10-19 22:42:54 +0200

    Upraveny UX v MobileModal.js

--- app/_components/MobileModal.js
+++ app/_components/MobileModal.js
@@ -22,24 +22,15 @@ export default function MobileModal({ children, onClose }) {
         <>
             {/* Overlay */}
             <div
-                className="fixed inset-0 z-50 bg-black/40 backdrop-blur-sm transition-opacity duration-300 items-center justify-center flex"
+                className="fixed inset-0 z-50 bg-black/40 backdrop-blur-sm transition-opacity duration-300 items-center"
                 onClick={onClose}
             />
 
             {/* Modal Container */}
             <div
                 role="dialog"
-                className="animate-in fade-in zoom-in fixed left-1/2 top-1/2 z-50 w-[80%] max-w-2xl -translate-x-1/2 -translate-y-1/2 scale-100 transform rounded-2xl opacity-100 transition-all duration-300 max-h-[100dvh] overflow-y-auto"  // ⬅️ jediné potrebné utility pre scroll
+                className="animate-in fade-in zoom-in fixed left-1/2 top-1/2 z-50 w-[90%] max-w-2xl -translate-x-1/2 -translate-y-1/2 scale-100 transform rounded-2xl opacity-100 duration-300 max-h-[100dvh] overflow-y-auto  bg-white/30  transition-opacity px-4 py-4"  // ⬅️ jediné potrebné utility pre scroll
             >
-                {/* Close Button */}
-                {/* <button
-                    className="absolute right-3 top-3 rounded-full p-2 text-gray-400 transition-all duration-200 hover:bg-gray-100 hover:text-gray-600"
-                    onClick={onClose}
-                    aria-label="Zatvoriť modal"
-                >
-                    <IoCloseOutline className="h-6 w-6" />
-                </button> */}
-
                 <div>{children}</div>
             </div>
         </>,
