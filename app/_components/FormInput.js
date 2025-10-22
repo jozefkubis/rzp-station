148bffcf2e33530632a2c74a68e13472e5a9ddf3
JozefK <164421631+jozefkubis@users.noreply.github.com>
2025-10-22 10:42:10 +0200

    Upravene formulare FormInput a FormSelect

--- app/_components/FormInput.js
+++ app/_components/FormInput.js
@@ -5,7 +5,7 @@ export default function FormInput({
   ...rest
 }) {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-1 items-center border-t border-gray-200 px-2 md:px-4 py-3">
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-1 items-center md:border-t md:border-gray-200 px-2 md:px-4 py-3">
       <div>
         {label && (
           <label htmlFor={id} className="text-sm md:text-md font-semibold md:font-bold text-primary-700 flex">
@@ -17,7 +17,7 @@ export default function FormInput({
       <div className="">
         <input
           id={id}
-          className={`text-sm md:text-md w-full rounded-md border px-4 py-1 md:py-2 text-primary-700 hover:cursor-pointer focus:outline-none focus:ring-2 focus:ring-primary-300 ${rest.disabled ? "bg-primary-50" : "bg-gray-50 font-semibold"
+          className={`text-sm md:text-md w-full rounded-md border px-4 py-2 text-primary-700 hover:cursor-pointer focus:outline-none focus:ring-2 focus:ring-primary-300 ${rest.disabled ? "bg-primary-50" : "bg-gray-50 font-semibold"
             } ${className}`}
           {...rest}
         />
