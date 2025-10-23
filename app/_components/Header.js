5a16af540831289cae0263d52a7ff0e226b15086
jozef kubis <kubis.jozef@outlook.com>
2025-10-23 15:31:13 +0200

    Kozmeticka uprava v MyButtons.js

--- app/_components/Header.js
+++ app/_components/Header.js
@@ -1,23 +1,26 @@
 // import Logo from "./Logo"
-import MobileHeaderNav from "./MobileHeaderNav"
-import Navigation from "./Navigation"
-import UserHeaderInfo from "./UserHeaderInfo"
+import MobileHeaderNav from "./MobileHeaderNav";
+import Navigation from "./Navigation";
+import UserHeaderInfo from "./UserHeaderInfo";
 
 function Header() {
-    return (
-        <>
-            <header data-cy="header" className="hidden md:block border-b border-primary-200 text-primary-50 px-8 py-1 sticky top-0 z-50 bg-gray-50">
-                <div className="flex justify-between items-center max-w-screen-xl mx-auto w-full">
-                    <UserHeaderInfo />
-                    <Navigation />
-                </div>
-            </header>
+  return (
+    <>
+      <header
+        data-cy="header"
+        className="sticky top-0 z-50 hidden border-b border-primary-200 bg-gray-50 px-8 py-1 text-primary-50 md:block"
+      >
+        <div className="mx-auto flex w-full max-w-screen-xl items-center justify-between">
+          <UserHeaderInfo />
+          <Navigation />
+        </div>
+      </header>
 
-            <div className="md:hidden">
-                <MobileHeaderNav />
-            </div>
-        </>
-    )
+      <div className="md:hidden">
+        <MobileHeaderNav />
+      </div>
+    </>
+  );
 }
 
-export default Header
+export default Header;
--- app/_components/calendar/MyButtons.js
+++ app/_components/calendar/MyButtons.js
@@ -1,63 +1,72 @@
 import {
-    Tooltip,
-    TooltipContent,
-    TooltipProvider,
-    TooltipTrigger,
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
 } from "@radix-ui/react-tooltip";
 import { FaRegCalendarPlus } from "react-icons/fa";
 import { LuCalendarHeart } from "react-icons/lu";
 
 export default function MyButtons({
-    setSelectedEvent,
-    setDraftSlot,
-    setIsOpenModal,
-    showHoliday,
-    setShowHoliday,
+  setSelectedEvent,
+  setDraftSlot,
+  setIsOpenModal,
+  showHoliday,
+  setShowHoliday,
 }) {
+  // ----- obslužná funkcia -----
+  function handleAdd() {
+    setSelectedEvent(null); // vynuluj vybratú udalosť
+    setDraftSlot(null); // vynuluj "draft" slot
+    setIsOpenModal(true); // otvor modal v režime PRIDAŤ
+    console.log("ADD"); // len na debug – môžeš zmazať
+  }
 
-    // ----- obslužná funkcia -----
-    function handleAdd() {
-        setSelectedEvent(null);   // vynuluj vybratú udalosť
-        setDraftSlot(null);       // vynuluj "draft" slot
-        setIsOpenModal(true);     // otvor modal v režime PRIDAŤ
-        console.log("ADD");       // len na debug – môžeš zmazať
-    }
+  return (
+    <div className="flex flex-col gap-2 md:z-10">
+      <TooltipProvider delayDuration={100}>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <button
+              className="rounded bg-primary-700 px-2 py-1 text-gray-50"
+              onClick={handleAdd}
+              aria-label="Pridať udalosť"
+              data-cy="calendar-add-event-button"
+            >
+              <FaRegCalendarPlus />
+            </button>
+          </TooltipTrigger>
+          <TooltipContent
+            side="right"
+            align="center"
+            className="ml-[-2px] cursor-pointer rounded-r-full bg-primary-700 px-4 py-0 font-semibold text-gray-50"
+            onClick={handleAdd}
+          >
+            Pridať udalosť
+          </TooltipContent>
+        </Tooltip>
 
-    return (
-        <div className="flex flex-col gap-2 md:z-50">
-            <TooltipProvider delayDuration={100}>
-                <Tooltip>
-                    <TooltipTrigger asChild>
-                        <button
-                            className="rounded bg-primary-700 text-gray-50 px-2 py-1"
-                            onClick={handleAdd}
-                            aria-label="Pridať udalosť"
-                            data-cy="calendar-add-event-button"
-                        >
-                            <FaRegCalendarPlus />
-                        </button>
-                    </TooltipTrigger>
-                    <TooltipContent side="right" align="center" className="text-gray-50 bg-primary-700 px-4 py-0 rounded-r-full ml-[-2px] cursor-pointer font-semibold" onClick={handleAdd}>
-                        Pridať udalosť
-                    </TooltipContent>
-                </Tooltip>
-
-                <Tooltip>
-                    <TooltipTrigger asChild>
-                        <button
-                            data-cy="calendar-toggle-holidays-button"
-                            className="rounded bg-primary-700 text-gray-50 px-2 py-1"
-                            onClick={() => setShowHoliday(!showHoliday)}
-                            aria-label={!showHoliday ? "Skryť sviatky" : "Zobraziť sviatky"}
-                        >
-                            <LuCalendarHeart />
-                        </button>
-                    </TooltipTrigger>
-                    <TooltipContent side="right" align="center" className="text-gray-50 bg-primary-700 px-4 py-0 rounded-r-full ml-[-2px] cursor-pointer font-semibold" onClick={() => setShowHoliday(!showHoliday)}>
-                        {!showHoliday ? "Skryť sviatky" : "Zobraziť sviatky"}
-                    </TooltipContent>
-                </Tooltip>
-            </TooltipProvider>
-        </div>
-    );
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <button
+              data-cy="calendar-toggle-holidays-button"
+              className="rounded bg-primary-700 px-2 py-1 text-gray-50"
+              onClick={() => setShowHoliday(!showHoliday)}
+              aria-label={!showHoliday ? "Skryť sviatky" : "Zobraziť sviatky"}
+            >
+              <LuCalendarHeart />
+            </button>
+          </TooltipTrigger>
+          <TooltipContent
+            side="right"
+            align="center"
+            className="ml-[-2px] cursor-pointer rounded-r-full bg-primary-700 px-4 py-0 font-semibold text-gray-50"
+            onClick={() => setShowHoliday(!showHoliday)}
+          >
+            {!showHoliday ? "Skryť sviatky" : "Zobraziť sviatky"}
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+    </div>
+  );
 }
