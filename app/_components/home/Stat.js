f6c761a77b84402c1a95919a3c13142f366c6c15
JozefK <164421631+jozefkubis@users.noreply.github.com>
2025-11-13 10:34:21 +0100

    Este male upravy v Stat.js

--- app/_components/home/Stat.js
+++ app/_components/home/Stat.js
@@ -22,10 +22,10 @@ export default function Stat({ icon, title, value, color = "blue" }) {
       </div>
 
       {/* nadpis + hodnota */}
-      <h5 className="self-end font-semibold uppercase tracking-wide text-primary-700 md:text-[0.7rem] lg:text-[0.5rem] 2xl:text-[0.8rem]">
+      <h5 className="self-end font-semibold uppercase tracking-wide text-primary-700 md:text-[0.7rem] lg:text-[0.6rem] 2xl:text-[0.8rem]">
         {title}
       </h5>
-      <p className="text-sm font-medium leading-none text-primary-700 lg:text-[0.8rem] 2xl:text-[1.2rem]">
+      <p className="text-sm font-medium leading-none text-primary-700 lg:text-[0.9rem] 2xl:text-[1.2rem]">
         {value}
       </p>
     </div>
