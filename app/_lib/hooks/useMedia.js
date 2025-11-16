e976bddfb709ee20f6574e249251c3bec5272693
jozef kubis <kubis.jozef@outlook.com>
2025-11-16 13:14:54 +0100

    Uprava useMediaLarge min-width:1600px

--- app/_lib/hooks/useMedia.js
+++ app/_lib/hooks/useMedia.js
@@ -1,25 +1,25 @@
 import { useEffect, useState } from "react";
 
 export default function useMedia() {
-    const [isMd, setIsMdUp] = useState(false);
-    useEffect(() => {
-        const mql = window.matchMedia("(min-width: 768px)");
-        const update = () => setIsMdUp(mql.matches);
-        update();
-        mql.addEventListener?.("change", update);
-        return () => mql.removeEventListener?.("change", update);
-    }, []);
-    return isMd;
+  const [isMd, setIsMdUp] = useState(false);
+  useEffect(() => {
+    const mql = window.matchMedia("(min-width: 768px)");
+    const update = () => setIsMdUp(mql.matches);
+    update();
+    mql.addEventListener?.("change", update);
+    return () => mql.removeEventListener?.("change", update);
+  }, []);
+  return isMd;
 }
 
 export function useMediaLarge() {
-    const [isMdLarge, setIsMdLargeUp] = useState(false);
-    useEffect(() => {
-        const mql = window.matchMedia("(min-width: 1536px)");
-        const update = () => setIsMdLargeUp(mql.matches);
-        update();
-        mql.addEventListener?.("change", update);
-        return () => mql.removeEventListener?.("change", update);
-    }, []);
-    return isMdLarge;
+  const [isMdLarge, setIsMdLargeUp] = useState(false);
+  useEffect(() => {
+    const mql = window.matchMedia("(min-width: 1600px)");
+    const update = () => setIsMdLargeUp(mql.matches);
+    update();
+    mql.addEventListener?.("change", update);
+    return () => mql.removeEventListener?.("change", update);
+  }, []);
+  return isMdLarge;
 }
