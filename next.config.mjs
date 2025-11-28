6a1b127e2837cd48956682092386963d0d00d772
jozef kubis <kubis.jozef@outlook.com>
2025-11-28 16:21:01 +0100

    image unoptimazed spet na true...

--- next.config.mjs
+++ next.config.mjs
@@ -55,7 +55,7 @@ const nextConfig = {
     return [{ source: "/(.*)", headers: securityHeaders(isDev) }];
   },
   images: {
-    unoptimized: false,
+    unoptimized: true,
     remotePatterns: [
       // Supabase Storage – PUBLIC objekty
       {
