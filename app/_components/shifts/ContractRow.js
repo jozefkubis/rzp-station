bfac575c1b205a711e0d54114c8345b218be378f
jozef kubis <kubis.jozef@outlook.com>
2026-01-13 14:03:21 +0100

    Upravene mena a uvezok velkost

--- app/_components/shifts/ContractRow.js
+++ app/_components/shifts/ContractRow.js
@@ -2,7 +2,7 @@ export default function ContractRow({ children, cellBg, rowBg }) {
   return (
     <button
       type="button"
-      className={`flex h-10 cursor-pointer items-center justify-center border-l border-slate-200 hover:bg-blue-100 2xl:h-14 ${cellBg} ${rowBg} border-b border-slate-200 text-[0.65rem] 2xl:text-[0.9rem]`}
+      className={`flex h-10 cursor-pointer items-center justify-center border-l border-slate-200 hover:bg-blue-100 2xl:h-14 ${cellBg} ${rowBg} border-b border-slate-200 text-[0.6rem] 2xl:text-[0.85rem]`}
     >
       {children}
     </button>
